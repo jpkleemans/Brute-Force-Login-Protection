61f5d43ce4e2542ab56e4c8f9e0e9227cb63c84a
Jan-Paul Kleemans <jpkleemans@gmail.com>
2017-03-22 11:32:40 +0100

    Update whip

--- brute-force-login-protection.php
+++ brute-force-login-protection.php
@@ -12,7 +12,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2014-2015 Fresh-Media
+ * Copyright (c) 2014-2017 Fresh-Media
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
